8742e25242055b29da659b646ad40c18ce9189fe
Eduardo Echeverria <echevemaster@gmail.com>
2014-08-22 23:56:04 -0430

    Added home page

--- fudcon/ui/frontend/views.py
+++ fudcon/ui/frontend/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g, url_for
+from fudcon.modules.contents.models import Content
 
 bp = Blueprint('frontend', __name__,
                template_folder='templates')
@@ -7,5 +8,9 @@ bp = Blueprint('frontend', __name__,
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
+    queryset = Content.query.filter(Content.is_on_user_menu == 1,
+                                    Content.tag == 'home',
+                                    Content.active == 1).first_or_404()  
     return render_template('frontend/index.html',
-                           title="Home")
+                           title="Home",
+                           detail=queryset)
