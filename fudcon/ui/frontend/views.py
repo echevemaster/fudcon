a375662a9dfadcc3568daa2193ca2b4af46d4a62
Eduardo Echeverria <echevemaster@gmail.com>
2014-08-23 00:48:03 -0430

    change first_or_404 for first in views

--- fudcon/ui/frontend/views.py
+++ fudcon/ui/frontend/views.py
@@ -10,7 +10,7 @@ bp = Blueprint('frontend', __name__,
 def index():
     queryset = Content.query.filter(Content.is_on_user_menu == 1,
                                     Content.tag == 'home',
-                                    Content.active == 1).first_or_404()  
+                                    Content.active == 1).first()  
     return render_template('frontend/index.html',
                            title="Home",
                            detail=queryset)
