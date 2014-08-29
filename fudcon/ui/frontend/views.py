adea2fb95c1dd225d7a427943bfb70ce41d4d22a
Eduardo Echeverria <echevemaster@gmail.com>
2014-08-29 01:58:56 -0430

    Views for speakers

--- fudcon/ui/frontend/views.py
+++ fudcon/ui/frontend/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, g, url_for
 from fudcon.modules.contents.models import Content
+from fudcon.modules.speakers.models import Speaker
+from fudcon.app import app
 
 bp = Blueprint('frontend', __name__,
                template_folder='templates')
 
+items_per_page = app.config['ITEMS_PER_PAGE']
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
@@ -44,3 +47,13 @@ def sponsors():
     return render_template('frontend/index.html',
                            title='Sponsors',
                            detail=queryset)
+
+
+@bp.route('/speakers', methods=['GET', 'POST'])
+@bp.route('/speakers/<int:page>', methods=['GET', 'POST'])
+def speakers(page=1):
+    paginate_params = (page, items_per_page, False)
+    queryset = Speaker.query.paginate(*paginate_params)
+    return render_template('frontend/speakers.html',
+                           title=u'Ponentes',
+                           speakers=queryset)
