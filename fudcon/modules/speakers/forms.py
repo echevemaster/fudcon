fd92ccdcf5888508a80ab2260389edc608a475ea
Eduardo Echeverria <echevemaster@gmail.com>
2014-08-28 21:56:59 -0430

    Speaker form

---
+++ fudcon/modules/speakers/forms.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import Required
+
+
+class AddSpeaker(Form):
+    names = TextField(u'Nombres:',
+            [Required(u'Ingrese el nombre del ponente')])
+    fas = TextField(u'Cuenta FAS:',
+            [Required(u'Ingrese una cuenta FAS')])
+    bio = TextAreaField(u'Bio:',
+            [Required(u'Ingrese la bio del ponente')])
+
